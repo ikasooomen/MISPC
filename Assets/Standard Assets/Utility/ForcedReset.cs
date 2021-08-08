11c4b47f72366aaed64acbd2dcb1e61105630be8
ikasooomen <kaito20020711@gmail.com>
2021-08-09 03:34:36 +0900

    StandardAssetsのエラー修正

--- Assets/Standard Assets/Utility/ForcedReset.cs
+++ Assets/Standard Assets/Utility/ForcedReset.cs
@@ -4,6 +4,8 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityStandardAssets.CrossPlatformInput;
 
+using UnityEngine.UI;
+
 #pragma warning disable 618
 [RequireComponent(typeof (Image))]
 public class ForcedReset : MonoBehaviour
