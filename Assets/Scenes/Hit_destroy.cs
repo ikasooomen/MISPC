7c3baa72dfab3684b612331330e4a930ad2085a7
ikasooomen <kaito20020711@gmail.com>
2022-02-14 01:16:13 +0900

    no message

--- Assets/Scenes/Hit_destroy.cs
+++ Assets/Scenes/Hit_destroy.cs
@@ -11,47 +11,46 @@ public class Hit_destroy : MonoBehaviour
 
 	void Start()
     {
-		//�ʂ̐F�𓧖��ɂ���
+		
 		GetComponent<Renderer>().material.color = new Color32(0,0,0,0);
-		//�F��߂����������true�ɂȂ�
+		
 		tama_red = false;
 		deleteTime = 20f;
 	}
 
-	//�ʂ��Ȃ񂩂ɓ������������
+	
 	void OnCollisionEnter(Collision collision)
 	{
-		//�F���߂�Ă�
-		if(tama_red == true)
-        {
-			//�v���C���[��������e�ɓ��������
+
+		if (tama_red == true)
+		{
 			if (collision.gameObject.tag == "Bullet" || collision.gameObject.tag == "prpr")
 			{
-				//�ʂ���
+
 				Destroy(gameObject);
 			}
 
 
-		if (collision.gameObject.tag == "ATARIHANTEI")
-		{
-			float dmg = 0.25f;
-			Destroy(gameObject);
-			PlayerStats.Instance.TakeDamage(dmg);
+			if (collision.gameObject.tag == "ATARIHANTEI")
+			{
+				float dmg = 0.25f;
+				Destroy(gameObject);
+				PlayerStats.Instance.TakeDamage(dmg);
 
+			}
 		}
-		
 	}
 
 	void Update()
     {
-		//���Ԃ�0����J�E���g�A�b�v
+		
 		tama_timer+=Time.deltaTime;
-		//�ʂ���������Ă���1�b��ŁA�ʂ̐F�������ł��鎞
+		
         if (tama_timer >= 1f && tama_red == false)
         {
-			//�ʂ̐F��Ԃɂ���
+			
 			GetComponent<Renderer>().material.color = new Color32(255,0,0,255);
-			//�ʂ̐F���ԂɂȂ�����Ƃ�L�^
+
 			tama_red = true;
 			this.tag = "Bullet";
 		}
